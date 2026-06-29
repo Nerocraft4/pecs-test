57057752a84e10bcbf586ace1b0e95eea6b4ad38
Nerocraft4 <90830812+Nerocraft4@users.noreply.github.com>
2026-06-29 22:23:08 +0000

    feat: prototype fullscreen toggle

--- main.js
+++ main.js
@@ -52,3 +52,30 @@ function disableScroll() {
             window.scrollTo(scrollLeft, scrollTop);
         };
  }
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    console.log("fullscreening bro");
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+openFullscreen();
